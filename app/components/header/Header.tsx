3eb3311db90d39b2421e4f0d153ffd15a5c52dba
alexeirybak <rybak2108@yandex.ru>
2024-05-12 01:16:44 +0300

    modal

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { Login } from "@/app/login/Login";
+import { useState } from "react";
+import styles from "./Header.module.css";
 
 export const Header = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleLoginClick = () => {
+    setShowLogin(true);
+  };
+
   return (
     <header className="flex flex-row gap-y-5 justify-between px-[140px] mb-[60px] mt-12 mx-auto">
       <div className="flex flex-col gap-y-5">
@@ -12,9 +23,19 @@ export const Header = () => {
           Онлайн-тренировки для занятий дома
         </p>
       </div>
-      <button className="flex items-center justify-center w-32 h-14 p-4 rounded-full bg-custom-lime leading-110 text-center text-lg">
+      <button
+        className="flex items-center justify-center w-32 h-14 p-4 rounded-full bg-custom-lime leading-110 text-center text-lg"
+        onClick={handleLoginClick}
+      >
         Войти
       </button>
+      {showLogin && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modalContent}>
+            <Login />
+          </div>
+        </div>
+      )}
     </header>
   );
 };
